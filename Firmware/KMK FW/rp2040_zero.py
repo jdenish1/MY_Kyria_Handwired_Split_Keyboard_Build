cb9e2f30665b5f2fc7ec8d7333eca30b721934b0
jdenish1 <jareddenish@gmail.com>
2024-05-09 20:49:35 -0700

    new file:   Firmware/KMK FW/rp2040_zero.py

---
+++ Firmware/KMK FW/rp2040_zero.py
@@ -0,0 +1,27 @@
+import board rp2040_zero
+
+pinout = [
+    None,  # 5v
+    None,  # GND
+    None,  # 3.3v
+    board.29,
+    board.28,
+    board.27,
+    board.26,
+    board.15,
+    board.14,
+    board.13,
+    board.12,
+    board.11,
+    board.10,
+    board.9,
+    board.8,
+    board.7,
+    board.6,
+    board.5,
+    board.4,
+    board.3,
+    board.2,
+    board.TX,
+    board.RX,
+]
