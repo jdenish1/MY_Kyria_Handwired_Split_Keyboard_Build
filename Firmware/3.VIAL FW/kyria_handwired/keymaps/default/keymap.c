57ccf6b9168d5870c30116ba956f4b9c9526f2fe
jdenish1 <jareddenish@gmail.com>
2024-05-23 23:10:31 -0700

    modified:   Firmware/3.VIAL FW/kyria_handwired/keymaps/default/keymap.c 	new file:   Pics/Whole Keyboard Cropped-compressed.png 	modified:   Firmware/1.QMK FW/Firmware/Kyria_Handwired/Keymaps/jdenish1/keymap.c 	modified:   Firmware/1.QMK FW/Firmware/Kyria_Handwired/Keymaps/jdenish1/rules.mk 	Firmware/1.QMK FW/Firmware/Kyria_Handwired/OLD - TRASH/OLDinfo.json

--- Firmware/3.VIAL FW/kyria_handwired/keymaps/default/keymap.c
+++ Firmware/3.VIAL FW/kyria_handwired/keymaps/default/keymap.c
@@ -1,15 +1,31 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_1, KC_2,
-        KC_3, KC_4
-    ),
-
-    [1] = LAYOUT(
-        KC_A, KC_B,
-        KC_C, KC_D
+     /*
+      * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+      * │Tab│ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │Bsp│
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │Esc│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │ ' │
+      * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
+      * │Sft│ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │Sft│
+      * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘                                   
+      *                   ┌───┐           ┌───┐       
+      *       ┌───┐       │Spc├───┐   ┌───┤Spc│       ┌───┐
+      *       │Ent├───┐   └───┤Cap│   │Scl├───┘   ┌───┤Mut│
+      *       └───┤Gui├───┐   └───┘   └───┘   ┌───┤Ent├───┘
+      *           └───┤Win├───┐           ┌───┤Alt├───┘
+      *               └───┤Spc├───┐   ┌───┤Spc├───┘
+      *                   └───┤Del│   │Ent├───┘
+      *                       └───┘   └───┘         
+      * 
+      */
+    [0] = LAYOUT_split_3x6_5(
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,  KC_CAPS,         KC_SCRL, KC_SPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                   KC_ENT,  KC_LGUI, KC_LWIN, KC_SPC,  KC_DEL,          KC_ENT,  KC_SPC, KC_RALT, KC_RCTL, KC_MUTE
     )
 };
