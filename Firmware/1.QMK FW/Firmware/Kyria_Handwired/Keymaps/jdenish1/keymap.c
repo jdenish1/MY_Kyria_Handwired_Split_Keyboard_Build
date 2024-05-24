302d835bb99445357dda1cce671fbc6a9b3981ed
jdenish1 <jareddenish@gmail.com>
2024-05-23 23:59:25 -0700

    modified:   Firmware/1.QMK FW/Firmware/Kyria_Handwired/Keymaps/jdenish1/keymap.c 	modified:   Firmware/1.QMK FW/Firmware/Kyria_Handwired/Keymaps/jdenish1/rules.mk 	new file:   Firmware/1.QMK FW/Firmware/Kyria_Handwired/OLD - TRASH/OLDinfo.json 	modified:   Firmware/1.QMK FW/Firmware/kyria_handwired_default.uf2

--- Firmware/1.QMK FW/Firmware/Kyria_Handwired/Keymaps/jdenish1/keymap.c
+++ Firmware/1.QMK FW/Firmware/Kyria_Handwired/Keymaps/jdenish1/keymap.c
@@ -5,13 +5,13 @@
 
 enum layers {
     _QWERTY = 0,
-    _NUM,
-    _NAV,
-    _FKEY,
+    _NUM = 1,
+    _NAV = 2,
+    _FKEY = 3,
 };
 
 // Aliases for readability
-#define QWER     TO(_QWERTY)
+#define QWER     DF(_QWERTY)
 #define NUM      TG(_NUM)
 #define NAV      TG(_NAV)
 #define FKEY     TG(_FKEY)
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |(Enco)|      | Enter|      |      |  |      |      |      |      |(Enco)|
  *                        `----------------------------------'  `----------------------------------'
  */
-    [QWER] = LAYOUT(
+    [QWER] = LAYOUT_split_3x6_5(
       KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,  KC_T  ,                                        KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , KC_BSPC ,
       CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,  KC_G  ,                                        KC_H   , KC_J   , KC_K   , KC_L  , KC_SCLN, CTL_QUOT,
       KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,  KC_B  , KC_LBRC, KC_CAPS,       NUM , KC_RBRC, KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT ,
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |(Enco)|      | Enter|      |      |  |      |      |      |      |(Enco)|
  *                        `----------------------------------'  `----------------------------------'
  */
-    [NUM] = LAYOUT(
+    [NUM] = LAYOUT_split_3x6_5(
       KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_EQL ,
       KC_TILD, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
       KC_PIPE, KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LCBR,  QWER,    KC_TRNS, KC_RCBR, KC_RBRC, KC_UNDS, KC_COMM, KC_DOT , KC_SLSH, KC_QUES,
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                           | (Enco)|       | Enter |      |      |  |      |      |      |      |(Enco)|
  *                           `-------------------------------------'  `----------------------------------'
  */
-    [NAV] = LAYOUT(
+    [NAV] = LAYOUT_split_3x6_5(
       KC_TRNS, KC_LGUI, KC_LCTL , KC_LSFT, KC_TRNS, KC_TRNS,                                    KC_PGUP, KC_HOME, KC_UP  , KC_END , KC_VOLU, KC_DEL,
       KC_TRNS, RGB_SPI, RGB_MOD , RGB_VAI, RGB_HUI, RGB_SAI,                                    KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
       KC_TRNS, RGB_SPD, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, KC_TRNS,  QWER  ,    NUM, KC_TRNS, KC_PAUS, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
@@ -105,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        |(Enco)|      | Enter|      |      |  |      |      |      |      |(Enco)|
  *                        `----------------------------------'  `----------------------------------'
  */
-    [FKEY] = LAYOUT(
+    [FKEY] = LAYOUT_split_3x6_5(
       KC_TRNS, KC_F9, KC_F10,  KC_F11, KC_F12, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
       KC_TRNS, KC_F5, KC_F6 ,  KC_F7 , KC_F8 , KC_TRNS,                                     KC_TRNS, KC_RSFT, KC_RCTL, KC_RALT, KC_DEL , KC_TRNS,
       KC_TRNS, KC_F1, KC_F2 ,  KC_F3 , KC_F4 , KC_TRNS,  KC_TRNS,  QWER,     NUM  , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
--- Firmware/1.QMK FW/Firmware/Kyria_Handwired/Keymaps/jdenish1/rules.mk
+++ Firmware/1.QMK FW/Firmware/Kyria_Handwired/Keymaps/jdenish1/rules.mk
@@ -3,3 +3,6 @@ NKRO_ENABLE = yes
 STENO_ENABLE = yes
 DEBOUNCE_TYPE = sym_defer_pk
 KEY_OVERRIDE_ENABLE = yes
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
